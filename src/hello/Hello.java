c1e1ab0dd7558f02c8bbce5c7aae0e1bf80f8417
J. nathan Matias <natematias@gmail.com>
2011-10-23 00:08:45 -0400

    integrated processing. Tests passing. Can't run jar

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,13 +1,24 @@
 package hello;
 
-public class Hello {
+import processing.core.*;
+
+public class Hello extends PApplet{
+
+//    Stripe[] stripes = new Stripe[50];
+
+    public void setup(){
+    //    size(200,200);
+    }
 
     public String greet() {
         return "Hello, world";
     }
 
-    public static void main(String[] args) {
-        Hello h = new Hello();
-        System.out.println(h.greet());
+ //   public static void main(String[] args) {
+ //       Hello h = new Hello();
+ //       System.out.println(h.greet());
+ //   }
+    public void draw(){
+        System.out.println(greet());
     }
 }
