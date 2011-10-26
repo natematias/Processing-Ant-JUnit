32003bdd9709539aaf336d2aa4d2f42c663b948f
J. nathan Matias <natematias@gmail.com>
2011-10-25 23:04:35 -0400

    fixed main to properly run the Processing applet

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,21 +4,21 @@ import processing.core.*;
 
 public class Hello extends PApplet{
 
-//    Stripe[] stripes = new Stripe[50];
-
     public void setup(){
-    //    size(200,200);
+        size(200,200);
+        smooth();
     }
 
     public String greet() {
         return "Hello, world";
     }
 
- //   public static void main(String[] args) {
- //       Hello h = new Hello();
- //       System.out.println(h.greet());
- //   }
-    public void draw(){
+    public static void main(String args[]) {
+      PApplet.main(new String[] { "--present", "hello.Hello" });
+    }
+
+   public void draw(){
+        background(100);
         System.out.println(greet());
     }
 }
